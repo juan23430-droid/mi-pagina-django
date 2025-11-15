ae5873e9247eb8710356af73fea0f3b2c999956b
daylutsss <dayra24564@cbtis75.edu.mx>
2025-11-14 18:31:38 -0600

    olaj

--- mi_sitio/settings.py
+++ mi_sitio/settings.py
@@ -123,3 +123,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# mi_sitio/settings.py
+
+# Asegúrate de importar 'os' si usas esta sintaxis
+import os 
+# ... (código existente) ...
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# ESTA LÍNEA ES LA QUE FALTA y la que Render necesita:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
